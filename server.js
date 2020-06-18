8bbc52b9316eff92430a6169a904cedfaa6a2503
John W Somerville <john@johnlisa.us>
2020-06-18 13:09:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ mobileApp.tables.add('Service');
 mobileApp.tables.add('Tool'); 
 mobileApp.tables.add('Material'); 
 mobileApp.tables.add('ServiceItem'); 
-app.use(mobileApp);
+mobileApp.tables.add('Manufacturer'); 
 app.listen(process.env.PORT || 3000);
